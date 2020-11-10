0d2b6856050996171bf597668245a96760c79147
Tprezioso <tommyprezioso@gmail.com>
2020-11-10 13:57:23 -0500

    Initial Commit

---
+++ MakeADecision/MakeADecisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeADecisionApp.swift
+//  MakeADecision
+//
+//  Created by Thomas Prezioso on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct MakeADecisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
